491d52744693918bdd32439822ba904c4b1c47fe
Bhushan Rathod <48349208+stan-rathod@users.noreply.github.com>
2019-03-11 23:58:55 +0530

    commit 01

---
+++ assets/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+	$('.scrollspy').scrollSpy();
+	$('.modal').modal();
+	$('.materialboxed').materialbox();
+	$('.slider').slider();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
